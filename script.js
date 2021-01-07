6fc7ab1904a69411529d2129a3f80dad861f0722
TJJ101 <S10194152@connect.np.edu.sg>
2021-01-07 14:55:04 +0800

    commit

--- script.js
+++ script.js
@@ -23,11 +23,47 @@ $('#password, #confirmPass').on('keyup', function () {
       $('#warning').html('Password Does not Match').css('color', 'red');
 });
 
-  Apicart
+
+
+
+Apicart
   .setDevEnv() // Switch to dev environment
   .configure({
     store: new Apicart.Store({
       token: "LfkUIBRkw51xZcypQcDPc42GD7Vn0gGH?3d19cQlIS5o!!iqldLgyCA8tKYF_j12"
-    })
+    }),
+    payments: new Apicart.Payments({token: 
+      "bgYHvg2CPgd1yq6HrvIUkgab3Hsyx8!5gmaMWCSBjEI!a2A9cCgmrh?3d6k8i81l"
+    }),
+    vueComponentsTranslator: {
+      currencyFormats: {
+        en: {
+          currency: {
+            currency: 'SGD'
+          }
+        }
+      }
+    },
+    vueComponents: {
+      cartDropdown: {
+        dropdownPosition: 'right'
+      },
+      category: {
+        products: {
+          description: {
+            length: 250
+          },
+          list: [
+            "https://cdn.apicart.dev/external/kelmr738y22probf7pftjeufr1vahzyd/data/01/1.json",
+            "https://cdn.apicart.dev/external/kelmr738y22probf7pftjeufr1vahzyd/data/02/2.json",
+            "https://cdn.apicart.dev/external/kelmr738y22probf7pftjeufr1vahzyd/data/03/3.json",
+            "https://cdn.apicart.dev/external/kelmr738y22probf7pftjeufr1vahzyd/data/04/4.json",
+            "https://cdn.apicart.dev/external/kelmr738y22probf7pftjeufr1vahzyd/data/05/5.json",
+            "https://cdn.apicart.dev/external/kelmr738y22probf7pftjeufr1vahzyd/data/06/6.json"
+          ]
+        }
+      }
+    }
   })
   .initVueBundle();
+
