e8d8337526a0a016d1d0de130d97ab9928643c74
TJJ101 <S10194152@connect.np.edu.sg>
2021-01-10 13:22:50 +0800

    commit

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
-var regpopup = document.getElementById('register');
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == regpopup) {
-        regpopup.style.display = "none";
-    }
+// Back to Top Button Script
+window.onscroll = function() {scrollVisibility()};
+function scrollVisibility() {
+  if(document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+    document.getElementById("bttopbtn").style.display = "inline-block";
+  } else {
+    document.getElementById("bttopbtn").style.display = "none";
+  }
+}
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
 
+// Login/Register Script
+var regpopup = document.getElementById('register');
 var loginpopup = document.getElementById('login');
+// When the user clicks anywhere outside of the login/register popup,  it will close
 
-window.onclick = function(event){
-  if(event.target == loginpopup){
-    loginpopup.style.display = "none";
-  }
+window.onclick = function(event) {
+    if (event.target == regpopup)  {
+        regpopup.style.display = "none";
+    } else if (event.target == loginpopup) {
+      loginpopup.style.display = "none";
+    }
 }
 
 // To check if the password is matching on the register popup //
@@ -25,9 +35,9 @@ $('#password, #confirmPass').on('keyup', function () {
 
 
 
-
+/* API Javascript */
 Apicart
-  .setDevEnv() // Switch to dev environment
+  .setDevEnv()
   .configure({
     store: new Apicart.Store({
       token: "LfkUIBRkw51xZcypQcDPc42GD7Vn0gGH?3d19cQlIS5o!!iqldLgyCA8tKYF_j12"
@@ -36,6 +46,47 @@ Apicart
       "bgYHvg2CPgd1yq6HrvIUkgab3Hsyx8!5gmaMWCSBjEI!a2A9cCgmrh?3d6k8i81l"
     }),
     vueComponentsTranslator: {
+      localization: {
+        en: {
+          categories:{
+            trainingprogram: {
+              title: 'Training Programs',
+              description: "Best Training Programs",
+              menu: 'Training Programs',
+              new: {
+                title: 'New Programs',
+                description: "New Training Programs that are Available Currently",
+                menu: 'New Programs'
+              },
+              bundles: {
+                title: 'Special Bundle',
+                description: 'Special Bundle for Everyone',
+                menu: 'Special Bundles'
+              }
+            },
+            equipment: {
+              title: 'Training Equipments',
+              description: "High Quality Training Equipments",
+              menu: 'Training Equipments',
+              new:{
+                title: 'New Equipments',
+                description: "New Equipments that are Available Currently",
+                menu: 'New Equipments'
+              },
+              bundles: {
+                title: 'Special Bundles',
+                description: "Special Bundles for Everyone",
+                menu: 'Special Bundles'
+              }
+            },
+            limited: {
+              title: 'Limited Time Offer',
+              description: "Limited Time Offer for Everyone to Enjoy",
+              menu:  'Limited Time Offer'
+            }
+          }
+        }
+      },
       currencyFormats: {
         en: {
           currency: {
@@ -53,14 +104,31 @@ Apicart
           description: {
             length: 250
           },
-          list: [
-            "https://cdn.apicart.dev/external/kelmr738y22probf7pftjeufr1vahzyd/data/01/1.json",
-            "https://cdn.apicart.dev/external/kelmr738y22probf7pftjeufr1vahzyd/data/02/2.json",
-            "https://cdn.apicart.dev/external/kelmr738y22probf7pftjeufr1vahzyd/data/03/3.json",
-            "https://cdn.apicart.dev/external/kelmr738y22probf7pftjeufr1vahzyd/data/04/4.json",
-            "https://cdn.apicart.dev/external/kelmr738y22probf7pftjeufr1vahzyd/data/05/5.json",
-            "https://cdn.apicart.dev/external/kelmr738y22probf7pftjeufr1vahzyd/data/06/6.json"
-          ]
+          list: {
+            trainingprogram: {
+              new: [
+                "https://cdn.apicart.dev/external/kelmr738y22probf7pftjeufr1vahzyd/data/01/1.json",
+                "https://cdn.apicart.dev/external/kelmr738y22probf7pftjeufr1vahzyd/data/02/2.json",
+                "https://cdn.apicart.dev/external/kelmr738y22probf7pftjeufr1vahzyd/data/03/3.json"
+              ],
+              bundles: [
+                "https://cdn.apicart.dev/external/kelmr738y22probf7pftjeufr1vahzyd/data/07/7.json"
+              ]
+            },
+            equipment: {
+              new: [
+                "https://cdn.apicart.dev/external/kelmr738y22probf7pftjeufr1vahzyd/data/04/4.json",
+                "https://cdn.apicart.dev/external/kelmr738y22probf7pftjeufr1vahzyd/data/05/5.json",
+                "https://cdn.apicart.dev/external/kelmr738y22probf7pftjeufr1vahzyd/data/06/6.json"
+              ],
+              bundles: [
+                "https://cdn.apicart.dev/external/kelmr738y22probf7pftjeufr1vahzyd/data/08/8.json"
+              ]
+            },
+            limited: [
+              "https://cdn.apicart.dev/external/kelmr738y22probf7pftjeufr1vahzyd/data/09/9.json"
+            ]
+          }
         }
       }
     }
